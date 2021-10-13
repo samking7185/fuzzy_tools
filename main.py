8fef043c749f4e4b5f5ce91e3155ab89b3dfc953
samking7185 <47400683+samking7185@users.noreply.github.com>
2021-10-13 13:10:38 -0400

    Created repository to track usage and changes to fuzzy tools

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+main.py
+~~~~~~~~~~~~
+This shows the structure and how to call a FIS using fuzzy_control.py
+"""
+
+from fuzzy_control import FIS
+import time
+
+parameters = {
+    "MF1": [(0, 0, 3), (1, 4, 4)],
+    "MF2": [(0, 0, 2), (1, 2, 3), (2, 4, 4)],
+    "OUT": [(0, 0, 1), (0, 1, 1)],
+    "RULES": [0, 1, 1, 0, 1, 1],
+    "BOUNDS": [0, 1]
+}
+
+tic = time.perf_counter()
+single = FIS(parameters)
+
+out1 = single.compute(3, 2)
+toc = time.perf_counter()
+
+print(out1)
+print(toc - tic)
